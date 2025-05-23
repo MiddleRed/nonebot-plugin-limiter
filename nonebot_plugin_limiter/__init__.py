fbbca3faeb4eaed3521b15b0302c949836df75ae
middlered <98752512+MiddleRed@users.noreply.github.com>
2025-05-23 14:04:44 +0800

    Where is my  __init__.py

---
+++ nonebot_plugin_limiter/__init__.py
@@ -0,0 +1,28 @@
+from nonebot import require
+from nonebot.plugin import PluginMetadata, inherit_supported_adapters
+
+require("nonebot_plugin_uninfo")
+require("nonebot_plugin_alconna")
+require("nonebot_plugin_apscheduler")
+from .config import Config
+
+__plugin_meta__ = PluginMetadata(
+    name="命令冷却",
+    description="通用命令冷却限制器",
+    usage="",
+    type="library",
+    homepage="https://github.com/MiddleRed/nonebot-plugin-cooldown",
+    config=Config,
+    supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna", "nonebot_plugin_uninfo"),
+    extra={"author": "MiddleRed <middlered@outlook.com>"},
+)
+
+from .cooldown import Cooldown as Cooldown
+from .cooldown import SlidingWindowCooldown as SlidingWindowCooldown
+from .entity import CooldownEntity as CooldownEntity
+from .entity import GlobalScope as GlobalScope
+from .entity import PrivateScope as PrivateScope
+from .entity import PublicScope as PublicScope
+from .entity import SceneScope as SceneScope
+from .entity import UserSceneScope as UserSceneScope
+from .entity import UserScope as UserScope
